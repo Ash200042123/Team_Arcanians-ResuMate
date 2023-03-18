570dc05ebcada13de20bdaa672ab2be924073cce
Mizbaul Haque Maruf <mizbaulhaque@iut-dhaka.edu>
2023-03-18 17:40:12 +0600

    Main Body Added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { ResumeProvider } from "./Context";
+import "./App.css";
+// import Navbar from "./components/Layouts/Navbar";
+import Main from "./components/Main";
+import WebFont from "webfontloader";
+
+function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Pacifico", "Poppins"],
+      },
+    });
+  }, []);
+
+  return (
+    <>
+      <ResumeProvider>
+        {/* <Navbar /> */}
+        <Main />
+      </ResumeProvider>
+    </>
+  );
+}
+
+export default App;
